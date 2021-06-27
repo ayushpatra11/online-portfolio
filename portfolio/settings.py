a9c1f74e77860d9e3b949df3b989361dd6b5f71d
ayushpatra11 <ayushpatra11@gmail.com>
2021-06-27 12:41:04 +0530

    deploying

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'templates', 'build', 'static')
 STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = True
